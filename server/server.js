e600f1c5c915947c3db758fd7b310c9770be873f
BS HR System <bs-hr@example.com>
2026-02-09 16:57:06 +0900

    fix: CORS allowedHeaders에 ngrok-skip-browser-warning 추가

--- server/server.js
+++ server/server.js
@@ -96,7 +96,7 @@ app.use(
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning'],
   })
 );
 
